26a814af22fa9c4525f3e41efb187838115a32c5
EdsonLopesJr <dev_edson@outlook.com>
2023-03-13 11:44:22 -0300

    Update teste.js

--- sequelize/teste.js
+++ sequelize/teste.js
@@ -4,8 +4,46 @@ const sequelize = new Sequelize('teste','root', '123456', {
     dialect: "mysql"
 })
 
-sequelize.authenticate().then(function() {
-    console.log("Conectado com Sucesso")
-}).catch(function (erro) {
-    console.log("Falha na conexão: "+ erro)
+// sequelize.authenticate().then(function() {
+//     console.log("Conectado com Sucesso")
+// }).catch(function (erro) {
+//     console.log("Falha na conexão: "+ erro)
+// })
+
+const Postagem = sequelize.define('postagens', {
+    titulo: {
+        type: Sequelize.STRING
+    },
+
+    conteudo: {
+        type: Sequelize.TEXT
+    }
 })
+// Postagem.sync({force:true})
+// Postagem.create({
+//     titulo: "UM TITULO QUALQUER",
+//     conteudo: "Mundo da programação é algo muito legal, é uma area muito ampla, quem se esforça para entender isso, o céu é o limite."
+// })
+
+const Usuario = sequelize.define('usuarios', {
+    nome: {
+        type: Sequelize.STRING
+    },
+    sobrenome: {
+        type: Sequelize.STRING
+    },
+    idade: {
+        type: Sequelize.INTEGER
+    },
+    email: {
+        type: Sequelize.STRING
+    }
+})
+// Usuario.sync({force:true})
+// Usuario.create({
+//     nome: 'Edson',
+//     sobrenome: 'Lopes',
+//     idade: 22,
+//     email: 'dev_edson@outlook.com'
+// })
+
